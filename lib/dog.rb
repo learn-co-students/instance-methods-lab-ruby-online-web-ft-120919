263179eee964deaace82a5465310b13dc6d6da98
Ho Yin Keung <hykeung92@gmail.com>
2019-12-18 04:33:48 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,12 @@
+class Dog 
+end
+def bark
+  puts "Woof!"
+end
+def sit 
+  puts "The dog is sitting"
+end
+
+fido = Dog.new
+fido.bark
+fido.sit
---
+++ lib/person.rb
@@ -0,0 +1,8 @@
+class Person
+end
+def walk
+  puts "The person is walking"
+end
+def talk
+  puts "Hello World!"
+end
